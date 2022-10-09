8b5fa8c1bf1435e00a8a67c1d4df2ab5846a4ebe
bmcgarvey <bmcgarvey@wesdigital.com.au>
2022-10-09 17:40:20 +1100

    adds content

---
+++ content/suellan.js
@@ -0,0 +1,166 @@
+export default {
+  name: 'Suellan Leelawardana',
+  title: 'Enterprise Risk Management Consultant, Senior Manager',
+  contacts: [
+    {
+      name: 'Phone (US)',
+      href: 'tel:+16462494640',
+      display: '+1 (646) 249-4640'
+    },
+    {
+      name: 'linkedin',
+      href: 'https://linkedin.com/in/suellan-leelawardana',
+      display: 'linkedin.com/in/suellan-leelawardana'
+    },
+    {
+      name: 'email',
+      href: 'mailto:suellan.leelawardana@gmail.com',
+      display: 'suellan.leelawardana@gmail.com'
+    },
+    {
+      name: 'address',
+      display: 'New York, New York'
+    }
+  ],
+  experiences: [
+    {
+      title: 'Senior Manager',
+      place: 'EY',
+      location: 'Melbourne, Australia',
+      from: '10/18',
+      to: 'present',
+      details: [
+        'Subject Matter Resource for enterprise risk management for First Line and Second Line functions',
+        'This has involved supporting clients to develop and sustain their enterprise risk management programs, including reviewing their risk monitoring and reporting approaches, risk appetite and tolerance definition, and designing integrated risk management solutions',
+        'In this role I lead business development and sales, through building strategic relationships across multiple industries to secure future pipeline',
+        'I also lead formalised employee performance management and professional development programs, for five employees within the Risk Advisory team'
+      ]
+    },
+    {
+      title: 'Manager',
+      place: 'EY',
+      location: 'Melbourne, Australia',
+      from: '10/17',
+      to: '10/18',
+      details: [
+        'As project manager on projects for public and private sector clients, led multiple teams to execute: enterprise risk management maturity evaluations, risk management process uplift programs, operational and strategic risk identification and assessment workshops, risk appetite definition, internal audit planning and internal audit reviews'
+      ]
+    },
+    {
+      title: 'Consultant - Senior Consultant',
+      place: 'EY',
+      location: 'Melbourne, Australia',
+      from: '02/14',
+      to: '10/17',
+      details: [
+        'Performed internal audit reviews for public sector and private sector organizations',
+        'Conducted operational and compliance reviews to identify business improvements, and support execution of risk-based compliance programs',
+        'Documented high-quality reports for Audit Committee and Board audiences',
+        'Co-facilitated and documented risk identification and assessment workshops with Executive Management and Boards, as well as operational teams'
+      ]
+    }
+  ],
+  education: [
+    {
+      title: 'Admitted as an Australian Lawyer',
+      place: 'Supreme Court of Victoria',
+      location: 'Melbourne, Australia',
+      to: '2019'
+    },
+    {
+      title: 'Graduate Diploma in Legal Practice',
+      place: 'College of Law',
+      location: 'Melbourne, Australia',
+      to: '2017'
+    },
+    {
+      title: 'Bachelor of Laws',
+      place: 'Australian National University',
+      location: 'Canberra, Australia',
+      to: '2013',
+      details: [
+        'ANU School of Law Exchange program to University of Alabama, Tuscaloosa'
+      ]
+    },
+    {
+      title: 'Bachelor of Finance (Major in Capital Markets)',
+      place: 'Australian National University',
+      location: 'Canberra, Australia',
+      to: '2013'
+    }
+  ],
+  note: {
+    text:
+      'Note: I am eligible to work in the U.S. on an E-3 visa, which is a two-year visa available only to Australians and which can be renewed indefinitely. The process to obtain the visa is straightforward and is summarized ',
+    extraText:
+      'This visa does not require sponsorship and can be self-funded by the applicant.',
+    linkText: 'here',
+    href:
+      'https://static1.squarespace.com/static/5d24cbfb75f55e000100967f/t/5db43b192ca7a63a6d405c38/1572092697712/jonkarolczak.com+-+Summary+of+E-3+Visa+Process+for+Employers.pdf'
+  },
+  skills: [
+    {
+      name: 'Personal profile',
+      list: [
+        'A Senior Manager and team leader with Big Four management consulting experience, advising private and public sector organizations to:',
+        'improve, design and implement their enterprise risk management programs;',
+        'design and execute internal audit programs; and',
+        'better manage compliance to their legislative, regulatory and contractual obligations.'
+      ]
+    },
+    {
+      name: 'Career highlights',
+      list: [
+        'Consistently met Senior Manager business development and sales Key Performance Indicators, including for FY20. Repeatedly recognized for top performance through fast-tracked out-of-cycle promotions (Senior Consultant to Senior Manager within three years).',
+        'Consistently demonstrated ability to adapt to new contexts, by delivering positive client outcomes across four EY Risk Advisory sub-competencies: enterprise risk management, internal audit, technology risk and cybersecurity.',
+        'Maintained momentum through interstate office transfer from Canberra to Melbourne in 2014, as demonstrated by exceeding all individual Key Performance Indicators for FY15. This is despite entering a new team, advisory competency and market.',
+        'Several EY Advisory awards for high quality work, involvement in EY’s charity and volunteering foundation, and performance in pursuits.',
+        'In March 2019, admitted as an Australian Lawyer within the Australian State of Victoria.'
+      ]
+    },
+    {
+      name: 'Key Projects',
+      list: [
+        "Agile programme stream lead (Product Owner) for leading Health Insurer's global Information Security Transformation Programme to uplift its Cybersecurity Governance, Risk and Compliance function.",
+        'Project lead for multi-national, 40,000+ employee logistics company to develop its enterprise risk management framework, risk and internal audit operating model, and internal audit plan, involving consultation with Executive Leadership and Board.',
+        "Project lead for risk appetite definition with the Executive and Board of Australia's leading science and research organization to support effective decision-making.",
+        'Subject Matter Resource and Internal Audit lead for Internal Audit program for State government departments and public agencies.',
+        'Team member supporting delivery of a framework, to help the State Department of Education and Training to manage privacy risks associated with providing student and staff personal information to third party vendors.'
+      ]
+    }
+  ],
+  references: [
+    {
+      name: 'Mark Dingle',
+      position: 'Deloitte, Partner, Risk Advisory',
+      relationship: 'Supervisor',
+      mobile: '+61 (0) 438 379 262',
+      email: 'markrdingle@gmail.com',
+      linkedin: 'linkedin.com/in/markdingle'
+    },
+    {
+      name: 'John Koo',
+      position: 'EY, Director, Digital Risk Advisory',
+      relationship: 'Supervisor',
+      mobile: '+61 (0) 407 949 124',
+      email: 'john.koo@au.ey.com',
+      linkedin: 'linkedin.com/in/john-koo-70bab242'
+    },
+    {
+      name: 'Catherine Friday',
+      position: 'EY, Managing Partner, Oceania Government and Health Services',
+      relationship: 'Supervisor',
+      mobile: '+61 (0) 438 379 262',
+      email: 'catherine.friday@au.ey.com',
+      linkedin: 'linkedin.com/in/catherine-friday'
+    },
+    {
+      name: 'Sonya Mischefski',
+      position: 'Bupa, Delivery Manager, Security Transformation Programme',
+      relationship: 'Client',
+      mobile: '+61 (0) 424 593 430',
+      email: 'sonya.mischefski@bupa.com.au',
+      linkedin: 'linkedin.com/in/sonya-mischefski-55b76866'
+    }
+  ]
+};
