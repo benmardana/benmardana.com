8fbdd0f75bf1df3d6517829d60467d7e0ea3e85d
Ben McGarvey <b.mcgarvey.bm@gmail.com>
2023-01-11 14:16:20 +1100

    shows messages

--- functions/api/manualytics.ts
+++ functions/api/manualytics.ts
@@ -7,6 +7,14 @@ const belongsToArray = <TValue>(
 ): value is TValue => (allowedValues as ReadonlyArray<unknown>).includes(value);
 
 export namespace api {
+  type Request = PagesFunction<{
+    ManualyticsEventEnv: KVNamespace;
+    AUTH_KEY: string;
+    ACCOUNT_ID: string;
+    API_TOKEN: string;
+    NAMESPACE_ID: string;
+  }>;
+
   export const formFields = ['from', 'message', 'contact'] as const;
   type FormDataFields = Partial<Record<typeof formFields[number], string>>;
 
@@ -24,9 +32,7 @@ export namespace api {
     return response;
   };
 
-  export const onRequestPost: PagesFunction<{
-    ManualyticsEventEnv: KVNamespace;
-  }> = async (context) => {
+  export const onRequestPost: Request = async (context) => {
     try {
       const formData = parseFormData(await context.request.formData());
       const origin = new URL(context.request.url).origin;
@@ -57,6 +63,98 @@ export namespace api {
       });
     }
   };
+
+  export const onRequestGet: Request = async (context) => {
+    const auth = context.request.headers.get('authorization')?.split(' ')[1];
+
+    if (auth !== context.env.AUTH_KEY) {
+      return new Response(null, {
+        status: 401,
+        headers: {
+          [`WWW-Authenticate`]: 'Basic',
+        },
+      });
+    }
+
+    const KEYS_URL = (accountID: string, namespaceID: string) =>
+      `https://api.cloudflare.com/client/v4/accounts/${accountID}/storage/kv/namespaces/${namespaceID}/keys`;
+
+    const KEY_VALUE_URL = (
+      accountID: string,
+      namespaceID: string,
+      key: string
+    ) =>
+      `https://api.cloudflare.com/client/v4/accounts/${accountID}/storage/kv/namespaces/${namespaceID}/values/${key}`;
+
+    const keys = (
+      await (
+        await fetch(
+          KEYS_URL(context.env.ACCOUNT_ID, context.env.NAMESPACE_ID),
+          {
+            headers: {
+              Authorization: `Bearer ${context.env.API_TOKEN}`,
+              ['Content-Type']: 'application/json',
+            },
+            method: 'GET',
+          }
+        )
+      ).json<{ result: { name: string }[] }>()
+    ).result;
+
+    const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <title>benmcgarvey.com</title>
+  <meta name="description" content="benmcgarvey.com" />
+
+  <meta property="og:title" content="benmcgarvey.com" />
+  <meta property="og:description" content="benmcgarvey.com" />
+
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/favicon-16x16.png" />
+  <link rel="manifest" href="assets/site.webmanifest" />
+
+  <link rel="stylesheet" href="style.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/tables-min.css" crossorigin="anonymous">
+  <script src="https://unpkg.com/htmx.org@1.8.4" integrity="sha384-wg5Y/JwF7VxGk4zLsJEcAojRtlVp1FKKdGy1qN+OMtdq72WRvX/EdRdqg/LOhYeV" crossorigin="anonymous"></script>
+</head>
+<body>
+  <table class="pure-table">
+  <thead>
+    <tr>
+      <th>From</th>
+      <th>Contact</th>
+      <th>Message</th>
+      <th>Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    ${keys
+      .map(
+        ({ name }) =>
+          `<tr hx-get="/api/manualytics/${name}" hx-trigger="revealed">
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+      <td>...</td>
+    </tr>`
+      )
+      .join('\n')}
+  </tbody>
+  </table>
+</body>`;
+
+    return new Response(html, {
+      status: 200,
+      headers: {
+        'content-type': 'text/html;charset=UTF-8',
+      },
+    });
+  };
 }
 
 export const onRequestPost = api.onRequestPost;
+export const onRequestGet = api.onRequestGet;
---
+++ functions/api/manualytics/[id].ts
@@ -0,0 +1,90 @@
+type Request = PagesFunction<{
+  ManualyticsEventEnv: KVNamespace;
+  AUTH_KEY: string;
+  ACCOUNT_ID: string;
+  API_TOKEN: string;
+  NAMESPACE_ID: string;
+}>;
+
+const KEY_VALUE_URL = (
+  accountID: string,
+  namespaceID: string,
+  key: string | string[]
+) =>
+  `https://api.cloudflare.com/client/v4/accounts/${accountID}/storage/kv/namespaces/${namespaceID}/values/${key}`;
+
+export const onRequestDelete: Request = async (context) => {
+  const auth = context.request.headers.get('authorization')?.split(' ')[1];
+
+  if (auth !== context.env.AUTH_KEY) {
+    return new Response(null, {
+      status: 401,
+      headers: {
+        [`WWW-Authenticate`]: 'Basic',
+      },
+    });
+  }
+
+  const result = await fetch(
+    KEY_VALUE_URL(
+      context.env.ACCOUNT_ID,
+      context.env.NAMESPACE_ID,
+      context.params.id
+    ),
+    {
+      headers: {
+        Authorization: `Bearer ${context.env.API_TOKEN}`,
+        ['Content-Type']: 'application/json',
+      },
+      method: 'DELETE',
+    }
+  );
+
+  if (!result.ok) {
+    return new Response(null, { status: 500 });
+  }
+  return new Response(null, { status: 200 });
+};
+
+export const onRequestGet: Request = async (context) => {
+  const auth = context.request.headers.get('authorization')?.split(' ')[1];
+
+  if (auth !== context.env.AUTH_KEY) {
+    return new Response(null, {
+      status: 401,
+      headers: {
+        [`WWW-Authenticate`]: 'Basic',
+      },
+    });
+  }
+
+  const { from, message, contact } = await (
+    await fetch(
+      KEY_VALUE_URL(
+        context.env.ACCOUNT_ID,
+        context.env.NAMESPACE_ID,
+        context.params.id
+      ),
+      {
+        headers: {
+          Authorization: `Bearer ${context.env.API_TOKEN}`,
+          ['Content-Type']: 'application/json',
+        },
+        method: 'GET',
+      }
+    )
+  ).json<{ from: string; message: string; contact: string }>();
+
+  const html = `<td>${from}</td><td>${contact}</td><td>${message}</td><td>
+  <button class="btn btn-danger" hx-delete="/api/manualytics/${context.params.id}" hx-target="closest tr" hx-swap="outerHTML swap:1s">
+    Delete
+  </button>
+</td>`;
+
+  return new Response(html, {
+    status: 200,
+    headers: {
+      'content-type': 'text/html;charset=UTF-8',
+    },
+  });
+};
